29b2da609ee4b224e0029b1b6565201983845bfe
chullin <fg6ts15@gmail.com>
2024-03-29 23:42:00 +0800

    Added Telephone icon

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,16 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+
+var contactLink = document.getElementById('contact-link');
+
+// 添加點擊事件監聽器
+contactLink.addEventListener('click', function(event) {
+    // 阻止默認行為，即不跳轉頁面
+    event.preventDefault();
+    // 獲取電話資訊
+    var phoneNumber = contactLink.getAttribute('data-phone');
+    // 顯示電話資訊
+    alert('聯絡方式：' + phoneNumber);
+});
