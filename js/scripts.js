148f937028727a20d6fba5ef2694d9a31d3a1bc2
chullin <fg6ts15@gmail.com>
2024-03-29 16:51:37 +0800

    Added css for blog, second post, alert

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,14 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+
+var message = "此網站仍在建置中";
+
+// 檢查當前網頁的 URL
+if (window.location.href.indexOf("chullin.github.io/blog.html") > -1) {
+    // 如果 URL 包含 "chullin.github.io/blog.html"，顯示警告視窗
+    window.onload = function() {
+        alert(message);
+    };
+}
